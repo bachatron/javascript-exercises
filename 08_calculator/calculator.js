6bcfdacaca6a27b484d8708a28de956169a68af9
bachatron <idz420@gmail.com>
2022-09-07 20:49:27 -0300

    Calculator exercises done!

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,42 @@
-const add = function() {
+const add = function(n1, n2) {
+  return n1+n2;
+};
+
+const subtract = function(n1, n2) {
+  return n1 - n2;
+};
+
+const sum = function(array) {
+	let sum = 0
+  for (let num of array) {
+    sum += num;
+  }
+  return sum;  
+};
+
+const multiply = function(array) {
+  let multi = 1;
+  for (let num of array) {
+    multi *= num;
+  }
+  return multi;
+
+};
+
+const power = function(n1, n2) {
+  return n1 ** n2;
 	
 };
 
-const subtract = function() {
-	
-};
+const factorial = function(num) {
+  n = 1;
+  for (let i=1; i<=num; i++) {
+    console.log(n);
+    console.log(i);
+    n *= i; 
+  }
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+  return n;
 	
 };
 
