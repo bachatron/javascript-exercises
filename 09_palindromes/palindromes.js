d9535e9867b4f0eeb4902fbc1ba80b31036abae3
bachatron <idz420@gmail.com>
2022-09-07 23:35:24 -0300

    Palindromes exercise done.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const reversedWord = word.toLowerCase().split("").reverse().join("").replace(/[^a-z0-9]/gi, '');
+    return reversedWord === word.toLowerCase().replace(/[^a-z0-9]/gi, '');
 };
 
 // Do not edit below this line
