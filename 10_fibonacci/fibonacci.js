ff203fd5e800c12fe70ea9be05938b0e0dcf959f
bachatron <idz420@gmail.com>
2022-09-08 17:30:23 -0300

    Fibonacci exercise done.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+    let numArray = [0,1];
+    if (position >= 0){
+        for (let i = 0; i<position; i++) {
+            numArray.push(numArray[i] + numArray[i+1]);
+            //console.log(numArray) 
+        }
+        return numArray[position];
+    } else return ("OOPS");
 
 };
 
